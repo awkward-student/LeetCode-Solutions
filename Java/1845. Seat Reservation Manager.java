8a62ca4817e38e86797a0457cea57ac5aa4f6e33
Vishwas <im.vishwas.03@gmail.com>
2023-11-06 20:37:38 +0530

    Create 1845. Seat Reservation Manager.java

---
+++ Java/1845. Seat Reservation Manager.java
@@ -0,0 +1,76 @@
+/*
+# 1845. Seat Reservation Manager
+
+Design a system that manages the reservation state of n seats that are numbered from 1 to n.
+
+Implement the SeatManager class:
+
+SeatManager(int n) Initializes a SeatManager object that will manage n seats numbered from 1 to n. All seats are initially available.
+int reserve() Fetches the smallest-numbered unreserved seat, reserves it, and returns its number.
+void unreserve(int seatNumber) Unreserves the seat with the given seatNumber.
+ 
+
+Example 1:
+Input
+["SeatManager", "reserve", "reserve", "unreserve", "reserve", "reserve", "reserve", "reserve", "unreserve"]
+[[5], [], [], [2], [], [], [], [], [5]]
+Output
+[null, 1, 2, null, 2, 3, 4, 5, null]
+
+Explanation
+SeatManager seatManager = new SeatManager(5); // Initializes a SeatManager with 5 seats.
+seatManager.reserve();    // All seats are available, so return the lowest numbered seat, which is 1.
+seatManager.reserve();    // The available seats are [2,3,4,5], so return the lowest of them, which is 2.
+seatManager.unreserve(2); // Unreserve seat 2, so now the available seats are [2,3,4,5].
+seatManager.reserve();    // The available seats are [2,3,4,5], so return the lowest of them, which is 2.
+seatManager.reserve();    // The available seats are [3,4,5], so return the lowest of them, which is 3.
+seatManager.reserve();    // The available seats are [4,5], so return the lowest of them, which is 4.
+seatManager.reserve();    // The only available seat is seat 5, so return 5.
+seatManager.unreserve(5); // Unreserve seat 5, so now the available seats are [5].
+ 
+
+Constraints:
+
+1 <= n <= 105
+1 <= seatNumber <= n
+For each call to reserve, it is guaranteed that there will be at least one unreserved seat.
+For each call to unreserve, it is guaranteed that seatNumber will be reserved.
+At most 105 calls in total will be made to reserve and unreserve.
+*/
+
+
+
+
+
+class SeatManager {
+    static int seat;
+    static int[] seats;
+
+    public SeatManager(int n) {
+        seats = new int[n];
+        seat = 0;
+        Arrays.fill(seats, -1);
+    }
+    
+    public int reserve() {
+        seats[seat] = 1;
+        int ticket = seat+1;
+        while(seat < seats.length && seats[seat] == 1){
+            seat++;
+        }
+        return ticket;
+    }
+    
+    public void unreserve(int seatNumber) {
+        seats[seatNumber-1] = -1;
+
+        seat = Math.min(seatNumber-1, seat);
+    }
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager obj = new SeatManager(n);
+ * int param_1 = obj.reserve();
+ * obj.unreserve(seatNumber);
+ */
