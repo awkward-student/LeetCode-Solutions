5924f637c08afe4c2c305dd09911aa9431db5d2e
Vishwas <im.vishwas.03@gmail.com>
2024-02-06 18:35:38 +0530

    Create 49. Group Anagrams.java

---
+++ Java/49. Group Anagrams.java
@@ -0,0 +1,46 @@
+/*
+# 49. Group Anagrams
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+*/
+
+
+
+
+
+class Solution {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    Map<String, List<String>> keyToAnagrams = new HashMap<>();
+
+    for (final String str : strs) {
+      char[] chars = str.toCharArray();
+      Arrays.sort(chars);
+      String key = String.valueOf(chars);
+      keyToAnagrams.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+    }
+
+    return new ArrayList<>(keyToAnagrams.values());
+  }
+}
