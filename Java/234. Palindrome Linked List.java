cc04ed587e1dcf856e5272aaa335d2dd92aa8768
Vishwas <im.vishwas.03@gmail.com>
2024-03-22 10:51:03 +0530

    Create 234. Palindrome Linked List.java

---
+++ Java/234. Palindrome Linked List.java
@@ -0,0 +1,76 @@
+/*
+# 234. Palindrome Linked List
+
+Given the head of a singly linked list, return true if it is a 
+palindrome or false otherwise.
+
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+Input: head = [1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+ 
+
+Follow up: Could you do it in O(n) time and O(1) space?
+*/
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (fast != null)
+            slow = slow.next;
+        slow = reverseList(slow);
+
+        while (slow != null) {
+            if (slow.val != head.val)
+                return false;
+            slow = slow.next;
+            head = head.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
