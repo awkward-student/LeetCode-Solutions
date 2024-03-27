ca499599346f66869f9e7cd822ccbf65f9369220
Vishwas <im.vishwas.03@gmail.com>
2024-03-27 20:16:13 +0530

    Create 713. Subarray Product Less Than K.java

---
+++ Java/713. Subarray Product Less Than K.java
@@ -0,0 +1,48 @@
+/*
+# 713. Subarray Product Less Than K
+
+Given an array of integers nums and an integer k, return the number of contiguous 
+subarrays where the product of all the elements in the subarray is strictly less than k.
+
+
+Example 1:
+Input: nums = [10,5,2,6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are:
+[10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+
+Example 2:
+Input: nums = [1,2,3], k = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+1 <= nums[i] <= 1000
+0 <= k <= 106
+*/
+
+
+
+
+
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1)
+            return 0;
+
+        int ans = 0;
+        int prod = 1;
+
+        for (int l = 0, r = 0; r < nums.length; ++r) {
+            prod *= nums[r];
+            while (prod >= k)
+                prod /= nums[l++];
+            ans += r - l + 1;
+        }
+
+        return ans;
+    }
+}
