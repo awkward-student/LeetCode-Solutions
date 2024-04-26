6c7156366583e49eb571cf87ea7fa5739dd2b411
Vishwas <im.vishwas.03@gmail.com>
2024-04-26 18:15:25 +0530

    Create 2370. Longest Ideal Subsequence.java

---
+++ Java/2370. Longest Ideal Subsequence.java
@@ -0,0 +1,60 @@
+/*
+# 2370. Longest Ideal Subsequence
+
+
+You are given a string s consisting of lowercase letters and an integer k. We call a string t ideal if the following conditions are satisfied:
+
+t is a subsequence of the string s.
+The absolute difference in the alphabet order of every two adjacent letters in t is less than or equal to k.
+Return the length of the longest ideal string.
+
+A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+
+Note that the alphabet order is not cyclic. For example, the absolute difference in the alphabet order of 'a' and 'z' is 25, not 1.
+
+ 
+Example 1:
+Input: s = "acfgbd", k = 2
+Output: 4
+Explanation: The longest ideal string is "acbd". The length of this string is 4, so 4 is returned.
+Note that "acfgbd" is not ideal because 'c' and 'f' have a difference of 3 in alphabet order.
+
+Example 2:
+Input: s = "abcd", k = 3
+Output: 4
+Explanation: The longest ideal string is "abcd". The length of this string is 4, so 4 is returned.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+0 <= k <= 25
+s consists of lowercase English letters.
+*/
+
+
+
+
+
+class Solution {
+    public int longestIdealString(String s, int k) {
+        // dp[i] := the longest subsequence that ends in ('a' + i)
+        int[] dp = new int[26];
+
+        for (final char c : s.toCharArray()) {
+            final int i = c - 'a';
+            dp[i] = 1 + getMaxReachable(dp, i, k);
+        }
+
+        return Arrays.stream(dp).max().getAsInt();
+    }
+
+    private int getMaxReachable(int[] dp, int i, int k) {
+        final int first = Math.max(0, i - k);
+        final int last = Math.min(25, i + k);
+        int maxReachable = 0;
+        for (int j = first; j <= last; ++j)
+            maxReachable = Math.max(maxReachable, dp[j]);
+        return maxReachable;
+    }
+}
