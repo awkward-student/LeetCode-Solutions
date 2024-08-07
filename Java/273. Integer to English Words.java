9ae8b63804e296790b6ef1be9e63fdd0821f9330
Vishwas <im.vishwas.03@gmail.com>
2024-08-07 10:42:21 +0530

    Create 273. Integer to English Words.java

---
+++ Java/273. Integer to English Words.java
@@ -0,0 +1,60 @@
+/*
+# 273. Integer to English Words
+
+
+Convert a non-negative integer num to its English words representation.
+ 
+Example 1:
+Input: num = 123
+Output: "One Hundred Twenty Three"
+
+Example 2:
+Input: num = 12345
+Output: "Twelve Thousand Three Hundred Forty Five"
+
+Example 3:
+Input: num = 1234567
+Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+ 
+
+Constraints:
+
+0 <= num <= 231 - 1
+*/
+
+
+
+
+
+
+class Solution {
+    public String numberToWords(int num) {
+        return num == 0 ? "Zero" : helper(num);
+    }
+
+    private final String[] belowTwenty = { "", "One", "Two", "Three", "Four",
+            "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+            "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+    private final String[] tens = { "", "", "Twenty", "Thirty", "Forty",
+            "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+    private String helper(int num) {
+        StringBuilder s = new StringBuilder();
+
+        if (num < 20)
+            s.append(belowTwenty[num]);
+        else if (num < 100)
+            s.append(tens[num / 10]).append(" ").append(belowTwenty[num % 10]);
+        else if (num < 1000)
+            s.append(helper(num / 100)).append(" Hundred ").append(helper(num % 100));
+        else if (num < 1000000)
+            s.append(helper(num / 1000)).append(" Thousand ").append(helper(num % 1000));
+        else if (num < 1000000000)
+            s.append(helper(num / 1000000)).append(" Million ").append(helper(num % 1000000));
+        else
+            s.append(helper(num / 1000000000)).append(" Billion ").append(helper(num % 1000000000));
+
+        return s.toString().trim();
+    }
+}
