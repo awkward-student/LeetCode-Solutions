8e7eb32452c95724b353e9d8d9446b81956fc3c8
Vishwas <im.vishwas.03@gmail.com>
2024-09-11 17:49:08 +0530

    Update 725. Split Linked List in Parts.java

--- Java/725. Split Linked List in Parts.java
+++ Java/725. Split Linked List in Parts.java
@@ -1,15 +1,16 @@
 /*
 # 725. Split Linked List in Parts
 
+
 Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
 
-The length of each part should be as equal as possible: no two parts should have a size differing by more than one. This may lead to some parts being null.
+The length of each part should be as equal as possible: no two parts should have a size differing by more than one. 
+This may lead to some parts being null.
 
-The parts should be in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
+The parts should be in the order of occurrence in the input list, 
+and parts occurring earlier should always have a size greater than or equal to parts occurring later.
 
 Return an array of the k parts.
-
- 
 
 Example 1:
 Input: head = [1,2,3], k = 5
@@ -34,6 +35,9 @@ The number of nodes in the list is in the range [0, 1000].
 
 
 
+
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -44,6 +48,7 @@ The number of nodes in the list is in the range [0, 1000].
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
   public ListNode[] splitListToParts(ListNode root, int k) {
     ListNode[] ans = new ListNode[k];
