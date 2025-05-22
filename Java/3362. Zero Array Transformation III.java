17ecd4a18da32c28e2f0faac7a53837eac788717
Vishwas <im.vishwas.03@gmail.com>
2025-05-22 18:19:02 +0530

    Create 3362. Zero Array Transformation III.java

---
+++ Java/3362. Zero Array Transformation III.java
@@ -0,0 +1,89 @@
+/*
+# 3362. Zero Array Transformation III
+
+
+You are given an integer array nums of length n and a 2D array queries where queries[i] = [li, ri].
+
+Each queries[i] represents the following action on nums:
+
+Decrement the value at each index in the range [li, ri] in nums by at most 1.
+The amount by which the value is decremented can be chosen independently for each index.
+A Zero Array is an array with all its elements equal to 0.
+
+Return the maximum number of elements that can be removed from queries, such that nums can still be converted to a zero array using the remaining queries. If it is not possible to convert nums to a zero array, return -1.
+ 
+
+Example 1:
+
+Input: nums = [2,0,2], queries = [[0,2],[0,2],[1,1]]
+
+Output: 1
+
+Explanation:
+
+After removing queries[2], nums can still be converted to a zero array.
+
+Using queries[0], decrement nums[0] and nums[2] by 1 and nums[1] by 0.
+
+Using queries[1], decrement nums[0] and nums[2] by 1 and nums[1] by 0.
+
+
+Example 2:
+
+Input: nums = [1,1,1,1], queries = [[1,3],[0,2],[1,3],[1,2]]
+
+Output: 2
+
+Explanation:
+
+We can remove queries[2] and queries[3].
+
+
+Example 3:
+
+Input: nums = [1,2,3,4], queries = [[0,3]]
+
+Output: -1
+
+Explanation:
+
+nums cannot be converted to a zero array even after using all the queries.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 105
+1 <= queries.length <= 105
+queries[i].length == 2
+0 <= li <= ri < nums.length
+*/
+
+
+
+
+
+class Solution {
+    public int maxRemoval(int[] nums, int[][] queries) {
+        Arrays.sort(queries, (a, b) -> Integer.compare(a[0], b[0]));
+        PriorityQueue<Integer> available = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> assigned = new PriorityQueue<>();
+        int count = 0;
+        for (int time = 0, k = 0; time < nums.length; time++) {
+            while (!assigned.isEmpty() && assigned.peek() < time)
+                assigned.poll();
+            while (k < queries.length && queries[k][0] <= time)
+                available.add(queries[k++][1]);
+            while (assigned.size() < nums[time]
+                   && !available.isEmpty()
+                   && available.peek() >= time) {
+                assigned.add(available.poll());
+                count++;
+            }
+            if (assigned.size() < nums[time])
+                return -1;
+        }
+        return queries.length - count;
+    }
+}
